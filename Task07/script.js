37cea62e1e75e0bd3d82a74b58710bf46ae658e6
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-08-16 16:49:05 -0300

    Change Profile

---
+++ Task07/script.js
@@ -0,0 +1,8 @@
+function trocar()
+{
+    const html = document.documentElement;
+    html.classList.toggle("light")
+
+    const body = document.getElementById("bb")
+    body.classList.toggle("lightBody")
+}
---
+++ Task08/script.js
@@ -0,0 +1,40 @@
+function changeImage()
+{
+    const html = document.documentElement;
+    html.classList.toggle("light")
+
+    var imagem = document.querySelector("#profile")
+    var dt = document.getElementById("data")
+    var paragraph = document.getElementById("p")
+    var gender = document.getElementById("gender")
+
+    var registro = new Date()
+    dt = registro
+
+    if(html.classList.contains("light"))
+    {
+        imagem.setAttribute("src", "./img1.png")
+        paragraph.style.color = "lightgreen"
+        document.getElementById("data").textContent = formatarData(dt)
+        gender.textContent = "Homem"
+    }
+    else
+    {
+        imagem.setAttribute("src", "./img2.png")
+        paragraph.style.color = "lightpink"
+        document.getElementById("data").textContent = formatarData(dt)
+        gender.textContent = "Mulher"
+    }
+}
+
+function formatarData(dt)
+{
+    var options = {
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    };
+    return dt.toLocaleString("pt-BR", options)
+}
